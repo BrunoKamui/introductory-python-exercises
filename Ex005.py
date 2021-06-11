c39416180d392856e8dba79bd30d2504e1de7646
Bruno Kamui <brunorbrasil@gmail.com>
2021-06-10 22:20:03 -0300

    ex005 and ex006

---
+++ Ex005.py
@@ -0,0 +1,28 @@
+#Usar as estruturas FOR e WHILE para ler 5 notas e informar a média
+
+#FOR
+
+soma = 0
+
+for i in range(1,6):
+  nota = float(input(f"Digite a nota da prova {i}: "))
+  soma += nota
+
+media = soma / 5
+
+print(f"A média do aluno foi {media}.")
+
+
+#WHILE
+
+provas = 1
+soma = 0
+
+while provas <= 5:
+  nota = float(input(f"Digite a nota da prova {provas}: "))
+  soma += nota
+  provas += 1
+
+media = soma / 5
+
+print(f"A média do aluno foi {media}.")
---
+++ Ex006.py
@@ -0,0 +1,16 @@
+#Usar as estruturas FOR e WHILE para imprimir a tabuada do número 3.
+
+#FOR
+
+for n in range(1,11):
+  print(f"3 x {n} = {3 * n}")
+
+
+
+#WHILE
+
+n = 1
+
+while n <= 10:
+  print(f"3 x {n} = {3 * n}")
+  n += 1
